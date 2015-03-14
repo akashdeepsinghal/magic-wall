0f0c8215fb0a7446c63d7ea6761a9af3fe6f43d5
akash <akigupta131@gmail.com>
2015-03-14 16:41:02 +0530

    background action set

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.wallpaper.setWallpaper({
+    'url': 'http://www.wallpaperup.com/uploads/wallpapers/2013/07/12/117504/big_thumb_cb8b67ada66c31930a7878f7d93e7f40.jpg',
+    'layout': 'CENTER_CROPPED',
+    'filename': 'test_wallpaper'
+}, function() {});
